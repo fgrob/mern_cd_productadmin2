69e120b28323154435978a37c5e67f96e89dda3a
unknown <fgrobc@gmail.com>
2023-02-02 11:41:32 -0300

    admin product parte 3

--- server/routes/adminprod.routes.js
+++ server/routes/adminprod.routes.js
@@ -4,4 +4,6 @@ module.exports = function(app){
     app.post('/api/crear', ProductController.createProduct);
     app.get('/api/products', ProductController.getProducts);
     app.get('/api/product/:id', ProductController.OneProduct);
+    app.put('/api/product/edit/:id', ProductController.editProduct);
+    app.delete('/api/product/delete/:id', ProductController.deleteProduct);
 }
